f07deb518d04b8749376f00b9039e2d9fb78e339
Yves Abi Raad <yves.abiraad@lau.edu.lb>
2024-05-09 23:22:04 +0300

    Added actor-search functionality

--- src/app/actor-search/actor-search.component.ts
+++ src/app/actor-search/actor-search.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActorService } from '../services/tmdb.service';
 @Component({
   selector: 'app-actor-search',
-  standalone: true,
-  imports: [],
   templateUrl: './actor-search.component.html',
-  styleUrl: './actor-search.component.css'
+  styleUrls: ['./actor-search.component.css']
 })
-export class ActorSearchComponent {
+export class ActorSearchComponent implements OnInit {
+  actors: any[] = [];
+  searchTerm: string = '';
 
+  constructor(private actorService: ActorService) {}
+
+  ngOnInit(): void {
+  }
+
+  search(): void {
+    if (this.searchTerm) {
+      this.actorService.searchActors(this.searchTerm).subscribe({
+        next: (response: any) => {
+          this.actors = response.results;
+        },
+        error: (err) => console.error('Error fetching actors:', err)
+      });
+    }
+  }
 }
