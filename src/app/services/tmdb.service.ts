e0f4ce1cd4e5869df7d1b16cca1405a23c5bf7b4
Yves Abi Raad <yves.abiraad@lau.edu.lb>
2024-05-09 23:18:57 +0300

    changed tmbd service

--- src/app/services/tmdb.service.ts
+++ src/app/services/tmdb.service.ts
@@ -1,12 +1,21 @@
-import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ApiService {
-  private apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZTkyYjk3ZDM4NmIzOTA1ODkwNmQ0ZjlmZmQwMmNjNCIsInN1YiI6IjY2Mjk4M2I2MjU4ODIzMDE2NDkyOTBjNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6dKMTiKqZ0lDIocJsqUKww9vo6n8H6Ep8I2NTRH_vC0'
+export class ActorService {
+  private baseUrl = `${environment.tmdbBaseUrl}/search/person`;
+  private apiKey = `?api_key=${environment.tmdbApiKey}`;
+
   constructor(private http: HttpClient) {}
 
+  searchActors(query: string) {
+    return this.http.get(`${this.baseUrl}${this.apiKey}&query=${query}&include_adult=false&language=en-US&page=1`);
+  }
+
+  getActorMovies(actorId: number) {
+    return this.http.get(`${environment.tmdbBaseUrl}/person/${actorId}/movie_credits${this.apiKey}&language=en-US`);
+  }
 }
