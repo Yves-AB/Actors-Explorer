2d4eae83f2e3239ba5ce99c9d6d4207fe01c1b3a
Yves Abi Raad <yves.abiraad@lau.edu.lb>
2024-05-09 23:01:46 +0300

    Added api key

--- src/app/services/tmdb.service.ts
+++ src/app/services/tmdb.service.ts
@@ -1,9 +1,12 @@
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TmdbService {
+export class ApiService {
+  private apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZTkyYjk3ZDM4NmIzOTA1ODkwNmQ0ZjlmZmQwMmNjNCIsInN1YiI6IjY2Mjk4M2I2MjU4ODIzMDE2NDkyOTBjNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6dKMTiKqZ0lDIocJsqUKww9vo6n8H6Ep8I2NTRH_vC0'
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
 }
