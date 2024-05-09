a1a17913695774ec95ad849136966e5bd0021675
Yves Abi Raad <yves.abiraad@lau.edu.lb>
2024-05-09 23:13:17 +0300

    added components

---
+++ src/app/actor-search/actor-search.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-actor-search',
+  standalone: true,
+  imports: [],
+  templateUrl: './actor-search.component.html',
+  styleUrl: './actor-search.component.css'
+})
+export class ActorSearchComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ActorSearchComponent } from './actor-search/actor-search.component';
+import { ActorDetailsComponent } from './actor-details/actor-details.component';
+import { FavoritesComponent } from './favorites/favorites.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/search', pathMatch: 'full' },
+  { path: 'search', component: ActorSearchComponent },
+  { path: 'actor/:id', component: ActorDetailsComponent },
+  { path: 'favorites', component: FavoritesComponent }
+];
