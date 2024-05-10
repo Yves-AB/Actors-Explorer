5d7a081f2f792632fb74624f53947a44681042b3
Yves Abi Raad <yves.abiraad@lau.edu.lb>
2024-05-10 15:21:30 +0300

    Fixed 3x3 pagination

--- src/app/actor-details/actor-details.component.ts
+++ src/app/actor-details/actor-details.component.ts
@@ -7,47 +7,51 @@ import { TmdbService } from '../services/tmdb.service';
   selector: 'app-actor-movies',
   standalone: true,
   imports: [CommonModule],
-templateUrl: './actor-details.component.html',
-styleUrl: './actor-details.component.css'
+  templateUrl: './actor-details.component.html',
+  styleUrls: ['./actor-details.component.css']
 })
-
 export class ActorDetailsComponent implements OnInit {
-  movies: any[] = [];
+  allMovies: any[] = []; // Stores all fetched movies
+  movies: any[] = []; // Stores movies for the current page
   actorId!: number;
   currentPage: number = 1;
+  pageSize: number = 9; // Number of items per page
 
   constructor(private tmdbService: TmdbService, private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
     this.route.params.subscribe(params => {
-      this.actorId = params['id'];
-      this.loadMovies(this.actorId, this.currentPage);
+      this.actorId = +params['id'];
+      this.loadMovies(this.actorId);
     });
   }
 
-  loadMovies(actorId: number, page: number): void {
+  loadMovies(actorId: number): void {
     this.tmdbService.getActorMovies(actorId).subscribe({
       next: (response) => {
-        console.log('Movies found: ', response);
-        this.movies = response.cast;
-        // Implement pagination logic here based on response and the required 3x3 format
+        this.allMovies = response.cast;
+        this.updatePage(1); // Start from the first page
       },
       error: (error) => console.error('Error fetching movies: ', error)
     });
   }
 
-  // Pagination methods
+  updatePage(page: number) {
+    this.currentPage = page;
+    const startIndex = (page - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.movies = this.allMovies.slice(startIndex, endIndex);
+  }
+
   nextPage() {
-    this.currentPage++;
-    this.loadMovies(this.actorId, this.currentPage);
+    if ((this.currentPage * this.pageSize) < this.allMovies.length) {
+      this.updatePage(this.currentPage + 1);
+    }
   }
 
   previousPage() {
     if (this.currentPage > 1) {
-      this.currentPage--;
-      this.loadMovies(this.actorId, this.currentPage);
+      this.updatePage(this.currentPage - 1);
     }
   }
 }
-
-
