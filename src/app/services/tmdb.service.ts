86cfe9a153b1ef35948773a542bbe178925a386c
Yves Abi Raad <yves.abiraad@lau.edu.lb>
2024-05-10 14:54:40 +0300

    fixed tmdb service to include httpheaders

--- src/app/services/tmdb.service.ts
+++ src/app/services/tmdb.service.ts
@@ -1,20 +1,21 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TmdbService {
-  private baseUrl = `$https://api.themoviedb.org/3/search/person`;
-  private apiKey = `?api_key=${'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZTkyYjk3ZDM4NmIzOTA1ODkwNmQ0ZjlmZmQwMmNjNCIsInN1YiI6IjY2Mjk4M2I2MjU4ODIzMDE2NDkyOTBjNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6dKMTiKqZ0lDIocJsqUKww9vo6n8H6Ep8I2NTRH_vC0'}`;
+  private apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZTkyYjk3ZDM4NmIzOTA1ODkwNmQ0ZjlmZmQwMmNjNCIsInN1YiI6IjY2Mjk4M2I2MjU4ODIzMDE2NDkyOTBjNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6dKMTiKqZ0lDIocJsqUKww9vo6n8H6Ep8I2NTRH_vC0';
 
   constructor(private http: HttpClient) {}
 
-  searchActors(query: string) {
-    return this.http.get(`${this.baseUrl}${this.apiKey}&query=${query}&include_adult=false&language=en-US&page=1`);
-  }
+  searchActors(query: string): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.apiKey}`
+    });
 
-  getActorMovies(actorId: number) {
-    return this.http.get(`$https://api.themoviedb.org/3/person/${actorId}/movie_credits${this.apiKey}&language=en-US`);
+    const url = 'https://api.themoviedb.org/3/search/person?query='+query+'&include_adult=false&language=en-US&page=1';
+    return this.http.get(url, { headers });
   }
 }
