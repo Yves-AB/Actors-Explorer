8fbff4a211743ff6acd8bcaf9df167ab7a96e7bf
Yves Abi Raad <yves.abiraad@lau.edu.lb>
2024-05-10 14:42:56 +0300

    changed service name

--- src/app/services/tmdb.service.ts
+++ src/app/services/tmdb.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class ActorService {
+export class TmdbService {
   private baseUrl = `$https://api.themoviedb.org/3/search/person`;
   private apiKey = `?api_key=${'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZTkyYjk3ZDM4NmIzOTA1ODkwNmQ0ZjlmZmQwMmNjNCIsInN1YiI6IjY2Mjk4M2I2MjU4ODIzMDE2NDkyOTBjNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6dKMTiKqZ0lDIocJsqUKww9vo6n8H6Ep8I2NTRH_vC0'}`;
 
