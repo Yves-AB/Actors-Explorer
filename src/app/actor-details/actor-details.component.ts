c26970eedb8f16db165ebcad6deea9c50182eb90
Yves Abi Raad <yves.abiraad@lau.edu.lb>
2024-05-10 15:13:15 +0300

    added actor details component

--- src/app/actor-details/actor-details.component.ts
+++ src/app/actor-details/actor-details.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TmdbService } from '../services/tmdb.service';
 
 @Component({
-  selector: 'app-actor-details',
+  selector: 'app-actor-movies',
   standalone: true,
-  imports: [],
-  templateUrl: './actor-details.component.html',
-  styleUrl: './actor-details.component.css'
+  imports: [CommonModule],
+templateUrl: './actor-details.component.html',
+styleUrl: './actor-details.component.css'
 })
-export class ActorDetailsComponent {
 
+export class ActorDetailsComponent implements OnInit {
+  movies: any[] = [];
+  actorId!: number;
+  currentPage: number = 1;
+
+  constructor(private tmdbService: TmdbService, private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.actorId = params['id'];
+      this.loadMovies(this.actorId, this.currentPage);
+    });
+  }
+
+  loadMovies(actorId: number, page: number): void {
+    this.tmdbService.getActorMovies(actorId).subscribe({
+      next: (response) => {
+        console.log('Movies found: ', response);
+        this.movies = response.cast;
+        // Implement pagination logic here based on response and the required 3x3 format
+      },
+      error: (error) => console.error('Error fetching movies: ', error)
+    });
+  }
+
+  // Pagination methods
+  nextPage() {
+    this.currentPage++;
+    this.loadMovies(this.actorId, this.currentPage);
+  }
+
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.loadMovies(this.actorId, this.currentPage);
+    }
+  }
 }
+
+
