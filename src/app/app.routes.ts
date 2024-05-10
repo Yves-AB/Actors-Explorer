b1c3edb4f76c4b34159cc414a8e2f9c8608ef030
Yves Abi Raad <yves.abiraad@lau.edu.lb>
2024-05-10 15:33:41 +0300

    fixed routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { ActorSearchComponent } from './actor-search/actor-search.component';
 import { ActorDetailsComponent } from './actor-details/actor-details.component';
 import { FavoritesComponent } from './favorites/favorites.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/search', pathMatch: 'full' },
+  { path: '', component: HomeComponent },  // Make HomeComponent the default landing page
   { path: 'search', component: ActorSearchComponent },
   { path: 'actor/:id', component: ActorDetailsComponent },
   { path: 'favorites', component: FavoritesComponent }
